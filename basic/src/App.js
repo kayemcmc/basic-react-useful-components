b2b6723a91e7a77945a599561d17a2d15c8fa37f
Karen Posada <k100posada@gmail.com>
2019-01-07 23:56:42 -0600

    Simple user context example

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment, createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import User from './User';
+import { UserContext } from './UserContext';
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Scott Tolinsky',
+    email: 'email@me.com'
+  };
+  logout = () => {
+    this.setState({
+      id: null,
+      name: '',
+      email: ''
+    });
+  };
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state,
+          logout: this.logout
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </a>
+            <User />
+          </header>
+        </div>
+      </UserProvider>
     );
   }
 }
